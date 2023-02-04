25487246be9d066a8f770d34c8ffe851ec2c781b
Marcus Leung <82466170+marcus-leung@users.noreply.github.com>
2023-02-04 13:57:10 -0800

    add camera

--- app.py
+++ app.py
@@ -1,12 +1,32 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, Response
 import os
+import cv2
 
 app = Flask(__name__)
+cap = cv2.VideoCapture(0)
+
+def get_frames():
+    while True:
+        success, frame = cap.read()
+
+        if not success:
+            break
+        else:
+            ret,buffer=cv2.imencode('.jpg',frame)
+            frame=buffer.tobytes()
+
+            yield (b'--frame\r\n'
+                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 @app.route('/home')
 @app.route('/')
 def home():
+
     return render_template('home.html')
+
+@app.route('/video')
+def video():
+    return Response(get_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 @app.route('/education')
 def education():
